8681a00488847ff48b1d72ca101f7f033a38115c
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-08-01 09:28:33 +0530

    Using MonogoDB Atlas for Deploying

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,28 @@
 const mongoose = require('mongoose');
-const port = process.env.MONGO_URI || 'mongodb://localhost/IssueTracker'; 
-mongoose.connect(port);
+const port = process.env.MONGO_URI; 
+// mongoose.connect(port);
 
-const db = mongoose.connection;
+const connectionParams={
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}
+
+mongoose.connect(port ,connectionParams)
+    .then( () => {
+        console.log('Connected to the database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. n${err}`);
+    })
+
+// const db = mongoose.connection;
 
 
-db.on('error', console.error.bind(console, 'Connection Error occur in DB'));
+// db.on('error', console.error.bind(console, 'Connection Error occur in DB'));
 
 
-db.once('open', ()=> {
-    console.log('Successfully connected to the DB');
-})
+// db.once('open', ()=> {
+//     console.log('Successfully connected to the DB');
+// })
 
-module.exports = db;
+// module.exports = db;
